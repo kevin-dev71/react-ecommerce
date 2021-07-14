59f1ec26d1cd1879a0c86523a85f276f44877b43
Kevin Dev <kevin.developer71@gmail.com>
2021-07-14 09:51:23 -0600

    cart-icon use context

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,12 +1,20 @@
-import shoppingBag from '../../assets/shopping-bag.png'
+import { useContext } from 'react'
+import { CartContext } from '../../context/cart-context'
 
+import shoppingBag from '../../assets/shopping-bag.png'
 import './cart-icon.styles.scss'
 
 const CartIcon = () => {
+  const { itemCount } = useContext(CartContext)
   return (
     <div className="cart-container">
       <img src={shoppingBag} alt="shopping-cart-icon" />
-      <span className="cart-count">5</span>
+      {
+        itemCount > 0 
+        ? <span className="cart-count">5</span> 
+        : null
+      }
+      
     </div>
   )
 }
